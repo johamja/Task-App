ffc75801c8492fa70b30f0cd69c51af7c1a8247b
Joham Jaider Morales Franco <johamfranco318@gmail.com>
2022-10-26 21:22:55 -0500

    Creating configuration layout and adding method to delete user

--- app/src/main/java/com/example/task_app/activities/HomeApp.java
+++ app/src/main/java/com/example/task_app/activities/HomeApp.java
@@ -2,17 +2,33 @@ package com.example.task_app.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import com.example.task_app.R;
+import com.example.task_app.model.firebase_authentication.User;
 
 public class HomeApp extends AppCompatActivity {
 
+    private ImageButton settings;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_app);
 
+        settings = findViewById(R.id.HomeApp_Button_settings);
+
+        settings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HomeApp.this,Settings.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
