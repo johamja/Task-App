0b85423086acc705d128b1b13c5f208211b75105
Daniel Elías <daniel.dominguez@upb.edu.co>
2022-11-01 13:51:50 -0500

    I added the news layouts and some functionalitys for categoryList and addCategory

--- app/src/main/java/com/example/task_app/activities/HomeApp.java
+++ app/src/main/java/com/example/task_app/activities/HomeApp.java
@@ -2,9 +2,13 @@ package com.example.task_app.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.task_app.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class HomeApp extends AppCompatActivity {
 
@@ -12,6 +16,19 @@ public class HomeApp extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_app);
+
+
+
+
+        Button intoCategory = findViewById(R.id.button2);
+
+        intoCategory.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                Intent intent = new Intent(HomeApp.this,CategoryList.class);
+                startActivity(intent);
+            }
+        });
 
 
     }
--- app/src/main/java/com/example/task_app/activities/MainActivity.java
+++ app/src/main/java/com/example/task_app/activities/MainActivity.java
@@ -1,11 +1,15 @@
-package com.example.task_app;
+package com.example.task_app.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-import com.example.task_app.activities.LogIn;
+import com.example.task_app.R;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,5 +18,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         startActivity(new Intent(MainActivity.this, LogIn.class ));
         finish();
+
+
+
+
+
     }
 }
