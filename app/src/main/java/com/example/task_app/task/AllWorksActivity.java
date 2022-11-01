c9cdad342e01644d4cb866ae8e6e582dfec3d5d5
josemiguel1014 <106204361+josemiguel1014@users.noreply.github.com>
2022-11-01 13:12:12 -0500

    button functionalities test

--- app/src/main/java/com/example/task_app/task/AllWorksActivity.java
+++ app/src/main/java/com/example/task_app/task/AllWorksActivity.java
@@ -1,16 +1,37 @@
 package com.example.task_app.task;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.task_app.R;
 
 public class AllWorksActivity extends AppCompatActivity {
-
+    Button btn_newtask;
+    ListView listWorks;
+    ArrayAdapter<String> adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_works);
+
+        btn_newtask = (Button) findViewById(R.id.btn_newtask);
+
+        btn_newtask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), NewWorkActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        listWorks = (ListView) findViewById(R.id.list_works);
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
+        listWorks.setAdapter(adapter);
     }
 }
